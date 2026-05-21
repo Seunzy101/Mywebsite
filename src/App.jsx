f501f972f8101f673d0502f203796a9361e18fcf
Seunzy101 <ashiruseun17@gmail.com>
2026-05-21 06:05:21 -0700

    changing the box name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Bookingbox from "./components/Bookingbox";
+import BookingBox from "./components/BookingBox";
 import Services from "./components/Services";
 import Destinations from "./components/Destinations";
 import Testimonials from "./components/Testimonials";
